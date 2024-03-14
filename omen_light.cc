e4bee5b8ee3b4f4675a49cfc0b2ce3fffe0e0cd8
Marco Ambrosio <marco.ambrosio@polito.it>
2024-03-14 16:02:46 +0100

    add all leds for desktop OMEN

--- omen_light.cc
+++ omen_light.cc
@@ -10,20 +10,43 @@ constexpr uint16_t PRODUCT_ID = 0x84fd;
 constexpr uint8_t VERSION = 0x12;
 constexpr uint8_t MAX_BRIGHTNESS = 100;
 
-struct __attribute__((packed)) Report {
-  enum class Mode : uint8_t { STATIC = 1, OFF = 5, BREATHING, CYCLE, BLINKING };
+struct __attribute__((packed)) Report
+{
+  enum class Mode : uint8_t
+  {
+    STATIC = 1,
+    OFF = 5,
+    BREATHING,
+    CYCLE,
+    BLINKING
+  };
 
-  enum class Type : uint8_t {
+  enum class Type : uint8_t
+  {
     OFF = 0,
     STATIC = 2,
     CHANGING = 10,
   };
 
-  enum class Led : uint8_t { FRONT = 1, CHASE = 2 };
+  enum class Led : uint8_t
+  {
+    FRONT = 1,
+    CASE = 2,
+    CPU = 4,
+    FAN_BOT = 5,
+    FAN_MID = 6,
+    FAN_TOP = 7,
+    ALL = 11
+  };
 
-  enum class Power : uint8_t { ON = 1, SUSPEND = 2 };
+  enum class Power : uint8_t
+  {
+    ON = 1,
+    SUSPEND = 2
+  };
 
-  enum class Theme : uint8_t {
+  enum class Theme : uint8_t
+  {
     OFF = 0,
     CUSTOM = 0,
     GALAXY,
@@ -32,25 +55,32 @@ struct __attribute__((packed)) Report {
     OCEAN
   };
 
-  enum class Speed : uint8_t { OFF = 0, SLOW, MEDIUM, FAST };
+  enum class Speed : uint8_t
+  {
+    OFF = 0,
+    SLOW,
+    MEDIUM,
+    FAST
+  };
 
-  struct __attribute__((packed)) Color {
+  struct __attribute__((packed)) Color
+  {
     uint8_t r;
     uint8_t g;
     uint8_t b;
   };
 
-  uint8_t report_id;  // always 0
+  uint8_t report_id; // always 0
   uint8_t reserved;
   uint8_t version;
   Mode mode;
-  uint8_t custom_color_count;  // total number of custom colors
-  uint8_t custom_color_id;     // the custom color to set. Starting with 1
+  uint8_t custom_color_count; // total number of custom colors
+  uint8_t custom_color_id;    // the custom color to set. Starting with 1
   uint8_t reserved2[2];
   Color front_color;
-  Color chase_color;
+  Color case_color;
   uint8_t reserved3[34];
-  uint8_t brightness;  // 0-100
+  uint8_t brightness; // 0-100
   Type type;
   uint8_t reserved4[4];
   Led led;
@@ -59,12 +89,13 @@ struct __attribute__((packed)) Report {
   Speed speed;
 };
 
-void ShowUsage(char* arg0) {
+void ShowUsage(char *arg0)
+{
   std::cout << R"Usage(
 Usage:
   omen_light <led> <power> <mode> [options..]
 
-  led:   the led module to control. Can be 'front', 'chase'
+  led:   the led module to control. Can be ['front', 'case', 'cpu', 'fan_bot', 'fan_mid', 'fan_top']
   power: the power state to which the setting is applied. Can be 'on', 'suspend'
   mode:  color mode. Can be 'off', 'static', 'breathing', 'cycle', 'blinking'
   
@@ -82,8 +113,8 @@ Options for breathing, cycle, and blinking mode:
 
 Example:
 
-# Turn off the chase led when the power is on.
-$ omen_light chase on off
+# Turn off the case led when the power is on.
+$ omen_light case on off
 
 # Set the front led to breath in volcano theme slowly when suspend
 $ omen_light front suspend breathing slow galaxy
@@ -94,27 +125,32 @@ $ omen_light front on blinking medium custom 255 0 0 0 0 255
 )Usage";
 }
 
-bool SendReport(const Report& report) {
-  if (hid_init()) {
+bool SendReport(const Report &report)
+{
+  if (hid_init())
+  {
     std::cerr << "hid_init() failed" << std::endl;
     return false;
   }
 
-  hid_device* handle = hid_open(VENDOR_ID, PRODUCT_ID, nullptr);
-  if (!handle) {
+  hid_device *handle = hid_open(VENDOR_ID, PRODUCT_ID, nullptr);
+  if (!handle)
+  {
     std::cerr << "hid_open() failed" << std::endl;
     return false;
   }
 
-  auto* report_ptr = reinterpret_cast<const uint8_t*>(&report);
-  if (hid_write(handle, report_ptr, sizeof(report)) < 0) {
+  auto *report_ptr = reinterpret_cast<const uint8_t *>(&report);
+  if (hid_write(handle, report_ptr, sizeof(report)) < 0)
+  {
     std::cerr << "hid_write() failed" << std::endl;
     return false;
   }
 
   hid_close(handle);
 
-  if (hid_exit()) {
+  if (hid_exit())
+  {
     std::cerr << "hid_exit() failed" << std::endl;
     return false;
   }
@@ -122,54 +158,88 @@ bool SendReport(const Report& report) {
   return true;
 }
 
-bool ParseLed(const std::string& arg, Report::Led* led) {
-  if (arg == "front") {
+bool ParseLed(const std::string &arg, Report::Led *led)
+{
+  if (arg == "front")
     *led = Report::Led::FRONT;
-  } else if (arg == "chase") {
-    *led = Report::Led::CHASE;
-  } else {
+  else if (arg == "case")
+    *led = Report::Led::CASE;
+  else if (arg == "cpu")
+    *led = Report::Led::CPU;
+  else if (arg == "fan_bot")
+    *led = Report::Led::FAN_BOT;
+  else if (arg == "fan_mid")
+    *led = Report::Led::FAN_MID;
+  else if (arg == "fan_top")
+    *led = Report::Led::FAN_TOP;
+  else if (arg == "all")
+    *led = Report::Led::ALL;
+  else
+  {
     std::cerr << "unknown led: " << arg << std::endl;
     return false;
   }
   return true;
 }
 
-bool ParsePower(const std::string& arg, Report::Power* power) {
-  if (arg == "on") {
+bool ParsePower(const std::string &arg, Report::Power *power)
+{
+  if (arg == "on")
+  {
     *power = Report::Power::ON;
-  } else if (arg == "suspend") {
+  }
+  else if (arg == "suspend")
+  {
     *power = Report::Power::SUSPEND;
-  } else {
+  }
+  else
+  {
     std::cerr << "unknown power: " << arg << std::endl;
     return false;
   }
   return true;
 }
 
-bool ParseMode(const std::string& arg, Report::Mode* mode) {
-  if (arg == "off") {
+bool ParseMode(const std::string &arg, Report::Mode *mode)
+{
+  if (arg == "off")
+  {
     *mode = Report::Mode::OFF;
-  } else if (arg == "static") {
+  }
+  else if (arg == "static")
+  {
     *mode = Report::Mode::STATIC;
-  } else if (arg == "breathing") {
+  }
+  else if (arg == "breathing")
+  {
     *mode = Report::Mode::BREATHING;
-  } else if (arg == "cycle") {
+  }
+  else if (arg == "cycle")
+  {
     *mode = Report::Mode::CYCLE;
-  } else if (arg == "blinking") {
+  }
+  else if (arg == "blinking")
+  {
     *mode = Report::Mode::BLINKING;
-  } else {
+  }
+  else
+  {
     std::cerr << "unknown mode: " << arg << std::endl;
     return false;
   }
   return true;
 }
 
-void ParseColor(char** argv, Report* report) {
-  Report::Color* c = nullptr;
-  if (report->led == Report::Led::FRONT) {
+void ParseColor(char **argv, Report *report)
+{
+  Report::Color *c = nullptr;
+  if (report->led == Report::Led::FRONT)
+  {
     c = &report->front_color;
-  } else {
-    c = &report->chase_color;
+  }
+  else
+  {
+    c = &report->case_color;
   }
 
   // Don't care about parsing error.
@@ -178,96 +248,128 @@ void ParseColor(char** argv, Report* report) {
   c->b = std::atoi(argv[2]);
 }
 
-bool ParseSpeed(const std::string& arg, Report::Speed* speed) {
-  if (arg == "slow") {
+bool ParseSpeed(const std::string &arg, Report::Speed *speed)
+{
+  if (arg == "slow")
     *speed = Report::Speed::SLOW;
-  } else if (arg == "medium") {
+  else if (arg == "medium")
     *speed = Report::Speed::MEDIUM;
-  } else if (arg == "fast") {
+  else if (arg == "fast")
     *speed = Report::Speed::FAST;
-  } else {
+  else
+  {
     std::cerr << "unknown speed: " << arg << std::endl;
     return false;
   }
   return true;
 }
 
-bool ParseTheme(const std::string& arg, Report::Theme* theme) {
-  if (arg == "custom") {
+bool ParseTheme(const std::string &arg, Report::Theme *theme)
+{
+  if (arg == "custom")
+  {
     *theme = Report::Theme::CUSTOM;
-  } else if (arg == "galaxy") {
+  }
+  else if (arg == "galaxy")
+  {
     *theme = Report::Theme::GALAXY;
-  } else if (arg == "volcano") {
+  }
+  else if (arg == "volcano")
+  {
     *theme = Report::Theme::VOLCANO;
-  } else if (arg == "jungo") {
+  }
+  else if (arg == "jungo")
+  {
     *theme = Report::Theme::JUNGO;
-  } else if (arg == "ocean") {
+  }
+  else if (arg == "ocean")
+  {
     *theme = Report::Theme::OCEAN;
-  } else {
+  }
+  else
+  {
     std::cerr << "unknown theme: " << arg << std::endl;
     return false;
   }
   return true;
 }
 
-bool Run(int argc, char** argv) {
-  Report report{};  // zero-initialized.
+bool Run(int argc, char **argv)
+{
+  Report report{}; // zero-initialized.
   report.version = VERSION;
 
-  if (argc < 4) return false;
-  if (!ParseLed(argv[1], &report.led)) return false;
-  if (!ParsePower(argv[2], &report.power)) return false;
-  if (!ParseMode(argv[3], &report.mode)) return false;
+  if (argc < 4)
+    return false;
+  if (!ParseLed(argv[1], &report.led))
+    return false;
+  if (!ParsePower(argv[2], &report.power))
+    return false;
+  if (!ParseMode(argv[3], &report.mode))
+    return false;
 
-  argc -= 4, argv += 4;  // consume arguments
-  switch (report.mode) {
-    case Report::Mode::OFF:
-      return SendReport(report);
+  argc -= 4, argv += 4; // consume arguments
+  switch (report.mode)
+  {
+  case Report::Mode::OFF:
+    return SendReport(report);
 
-    case Report::Mode::STATIC:
-      if (argc != 3) return false;
-      ParseColor(argv, &report);
+  case Report::Mode::STATIC:
+    if (argc != 3)
+      return false;
+    ParseColor(argv, &report);
+    report.custom_color_count = 1;
+    report.custom_color_id = 1;
+    report.brightness = MAX_BRIGHTNESS;
+    report.type = Report::Type::STATIC;
+    return SendReport(report);
+
+  case Report::Mode::BREATHING:
+  case Report::Mode::CYCLE:
+  case Report::Mode::BLINKING:
+    if (argc < 2)
+      return false;
+    if (!ParseSpeed(argv[0], &report.speed))
+      return false;
+    if (!ParseTheme(argv[1], &report.theme))
+      return false;
+
+    report.brightness = MAX_BRIGHTNESS;
+    report.type = Report::Type::CHANGING;
+
+    if (report.theme == Report::Theme::CUSTOM)
+    {
+      // custom theme
+      argc -= 2, argv += 2; // consume arguments
+      if (argc != 3 && argc != 6 && argc != 9 && argc != 12)
+        return false;
+      report.custom_color_count = argc / 3;
+      for (int i = 0; i < report.custom_color_count; ++i)
+      {
+        report.custom_color_id = i + 1;
+        ParseColor(argv + i * 3, &report);
+        if (!SendReport(report))
+          return false;
+      }
+      return true;
+    }
+    else
+    {
+      // pre-defined theme
       report.custom_color_count = 1;
       report.custom_color_id = 1;
-      report.brightness = MAX_BRIGHTNESS;
-      report.type = Report::Type::STATIC;
       return SendReport(report);
-
-    case Report::Mode::BREATHING:
-    case Report::Mode::CYCLE:
-    case Report::Mode::BLINKING:
-      if (argc < 2) return false;
-      if (!ParseSpeed(argv[0], &report.speed)) return false;
-      if (!ParseTheme(argv[1], &report.theme)) return false;
-
-      report.brightness = MAX_BRIGHTNESS;
-      report.type = Report::Type::CHANGING;
-
-      if (report.theme == Report::Theme::CUSTOM) {
-        // custom theme
-        argc -= 2, argv += 2;  // consume arguments
-        if (argc != 3 && argc != 6 && argc != 9 && argc != 12) return false;
-        report.custom_color_count = argc / 3;
-        for (int i = 0; i < report.custom_color_count; ++i) {
-          report.custom_color_id = i + 1;
-          ParseColor(argv + i * 3, &report);
-          if (!SendReport(report)) return false;
-        }
-        return true;
-      } else {
-        // pre-defined theme
-        report.custom_color_count = 1;
-        report.custom_color_id = 1;
-        return SendReport(report);
-      }
-      break;
+    }
+    break;
   }
 
   return true;
 }
 
-int main(int argc, char** argv) {
-  if (!Run(argc, argv)) {
+int main(int argc, char **argv)
+{
+  if (!Run(argc, argv))
+  {
     ShowUsage(argv[0]);
     return 1;
   }
